cd193a7a840c0d7d765baa263fe6b28412016eb5
simo-ennaim <mohammedennaim99@gmail.com>
2020-12-17 14:36:10 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+    const login = document.querySelector(".login")
+    const signup = document.querySelector(".signup")
+    const loginbtn = document.querySelector(".loginbtn")
+    const signupbtn = document.querySelector(".signupbtn")
+
+    signupbtn.addEventListener=("click", function(){
+    signup.style.marginLeft = "-50%";
+    });
